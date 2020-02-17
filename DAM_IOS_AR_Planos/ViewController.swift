8eb1e4a956abdf6492184e32687cca0cdae51167
raulrp3 <raul.ramirez@escuelaestech.es>
2020-02-17 16:44:54 +0100

    Creado juego "el suelo es lava"

--- DAM_IOS_AR_Planos/ViewController.swift
+++ DAM_IOS_AR_Planos/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet weak var mSceneview: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
@@ -19,9 +19,53 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         self.mSceneview.debugOptions = [.showWorldOrigin, .showFeaturePoints]
+        self.configuration.planeDetection = .horizontal
         self.mSceneview.session.run(configuration)
+        
+        self.mSceneview.delegate = self
     }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        //Detecta el plano horizontal.
+        guard let planeAnchor = anchor as? ARPlaneAnchor
+            else { return }
+        let lava = self.createLava(anchor: planeAnchor)
+        node.addChildNode(lava)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        //Actualiza el plano en caso de que ya haya sido detectado.
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        node.enumerateChildNodes{ (node, _) in
+            node.removeFromParentNode()
+        }
+        
+        let lava = self.createLava(anchor: planeAnchor)
+        node.addChildNode(lava)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        //Eliminar planos.
+        guard let _ = anchor as? ARPlaneAnchor else { return }
+        
+        node.enumerateChildNodes{ (node, _) in
+            node.removeFromParentNode()
+        }
+    }
+    
+    func createLava(anchor: ARPlaneAnchor) -> SCNNode{
+        let node = SCNNode(geometry: SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))) //Como el plano será girado, la altura la mide el eje z.
+        node.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "lava")
+        node.geometry?.firstMaterial?.isDoubleSided = true
+        node.position = SCNVector3(anchor.center.x, anchor.center.y, anchor.center.z)
+        node.eulerAngles = SCNVector3(90.degreesToRadians, 0, 0)
+        
+        return node
+    }
+}
 
-
+extension Int{
+    var degreesToRadians: Double { return Double(self) * .pi/180 }
 }
 
